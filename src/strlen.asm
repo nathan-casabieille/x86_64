3533daed45ca51fdd50a1dc1e068d69a61550660
nathan <nathan@localhost.localdomain>
2021-02-04 22:36:17 +0100

    strlen

---
+++ src/strlen.asm
@@ -0,0 +1,37 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;                                                          ;;
+;; x86-64 linux nasm assembly                               ;;
+;; Nathan Casabieille                                       ;;
+;; File description:                                        ;;
+;; Determines the length of a C-style NUL-terminated string ;;
+;;                                                          ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+section .text
+
+global strlen:function
+
+;; size_t strlen(char const *s)
+;; Inputs   :  RDI = offset string
+;; Outputs  :  EAX = length of the string, without the NULL terminator
+;; Clobbers :  rsi
+strlen:
+    push rbx
+    mov rbx, rdi
+    mov rsi, rbx
+
+    .repeat:
+        cmp byte [rbx], 0
+        je .done
+
+        inc rbx
+
+        jmp .repeat
+
+    .done:
+        sub rbx, rsi
+
+        mov rax, rbx
+
+        pop rbx
+        ret
