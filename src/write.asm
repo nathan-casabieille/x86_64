505ea0b59dc35804bd34e42524b6223e0a798156
nathan <nathan@localhost.localdomain>
2021-02-04 22:36:56 +0100

    write

---
+++ src/write.asm
@@ -0,0 +1,25 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;                                                                                                     ;;
+;; x86-64 linux nasm assembly                                                                          ;;
+;; Nathan Casabieille                                                                                  ;;
+;; File description:                                                                                   ;;
+;; Writes up to count bytes from the buffer pointed buf to the file referred to by the file descriptor ;;
+;;                                                                                                     ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+segment .bss
+
+SYS_WRITE: equ 1
+
+segment .text
+
+global write:function
+
+;; void write(int fd, const void *buf, size_t count);
+;; Inputs   :  RDI = file descriptor, RSI = offset string, RDX = number of characters to write
+;; Outputs  :  <none>
+;; Clobbers :  <none>
+write:
+    mov rax, SYS_WRITE
+    syscall
+    ret
